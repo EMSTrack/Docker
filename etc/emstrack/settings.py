f835a5b978fd79b564bf0274381577915cd10d11
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-12-08 10:11:19 -0800

    Adding logging settings

--- etc/emstrack/settings.py
+++ etc/emstrack/settings.py
@@ -194,3 +194,21 @@ MESSAGE_TAGS = {
     messages.ERROR: 'alert-danger',
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/var/log/django/debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
