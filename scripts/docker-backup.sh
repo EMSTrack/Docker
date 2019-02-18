215b13f6cde022d18a11dc627f3f126d1dec3c48
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-02-18 10:17:27 -0800

    docker-backup

--- scripts/docker-backup.sh
+++ scripts/docker-backup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]
+  then
+    echo "Please provide the name of the container"
+fi
+
 echo "> Copying fixtures"
-docker cp -a docker_emstrack_1:/etc/emstrack/fixtures etc/emstrack/
+docker cp -a $1:/etc/emstrack/fixtures etc/emstrack/
 
 echo "> Copying letsencrypt certificates"
-docker cp -a docker_emstrack_1:/etc/emstrack/letsencrypt etc/emstrack/
+docker cp -a $1:/etc/emstrack/letsencrypt etc/emstrack/
